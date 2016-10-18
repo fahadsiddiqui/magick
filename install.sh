40a5d6fb2aedf69948d526f1562b121e6a69b7cd
Fahad <fahad@functionize.com>
2016-10-19 00:48:32 +0500

    minor fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 echo "installing magick on your system..."
 apt-get -y install graphicsmagick
-mkdir $HOME/.magick
+mkdir -p $HOME/.magick
 cp magick.sh $HOME/.magick/
 chmod +x $HOME/.magick/magick.sh
-echo "\n$HOME/.magick/magick.sh" >> /etc/rc.local
+cp sample/magick.jpg $HOME/.magick/magick.jpg
+SOME_STRING="exit 0"
+PATH_TO_YOUR_FILE="/etc/rc.local"
+cat $PATH_TO_YOUR_FILE | head -n `expr $(echo $(cat -n $PATH_TO_YOUR_FILE | grep "$SOME_STRING" | tail -n 1) | cut -d ' ' -f 1) - 1` > $PATH_TO_YOUR_FILE
+echo "\n$HOME/.magick/magick.sh\n\n$SOME_STRING" >> $PATH_TO_YOUR_FILE
